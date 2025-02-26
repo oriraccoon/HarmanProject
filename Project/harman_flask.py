6fb52ccd463be6281aac0d7999b9e0ab8744d543
oriraccoon <djdjskskalal@gmail.com>
2025-02-26 09:29:59 +0900

    final

--- Project/harman_flask.py
+++ Project/harman_flask.py
@@ -5,18 +5,53 @@ import logging
 from flask import Flask, render_template, request, jsonify
 import json
 import os
+import requests
+
 
 # 이 상태로는 돌려도 안됨!! 경로 오류
 # 현재 json_file_path는 배포 파일 기준
 # 파이썬으로 구동하려면 ../삭제
+
+json_file_path = '../static/concert.json'
+
+# GitHub raw URL
+url = "https://raw.githubusercontent.com/taemin-ctrl/project/main/static/concert.json"
+
+# 파일 다운로드
+response = requests.get(url)
+# 기존 JSON 데이터 로드
+with open(json_file_path, 'r', encoding='UTF-8') as file:
+    original_data = json.load(file)
+
+# 요청이 성공한 경우 새 데이터 저장 및 병합
+if response.status_code == 200:
+    with open(json_file_path, 'wb') as file:
+        file.write(response.content)
+
+    # 새 데이터 로드
+    with open(json_file_path, 'r', encoding='UTF-8') as file:
+        update_data = json.load(file)
+
+    # 기존 데이터에 없는 객체만 추가
+    existing_names = {event["﻿이름"] for event in original_data}  # BOM 문자 주의
+    new_events = [event for event in update_data if event["﻿이름"] not in existing_names]
+
+    # 새로운 데이터 추가
+    original_data.extend(new_events)
+
+    # 업데이트된 JSON 다시 저장
+    with open(json_file_path, 'w', encoding='UTF-8') as file:
+        json.dump(original_data, file, ensure_ascii=False, indent=4)
+
+    print(f"파일이 {json_file_path}에 성공적으로 저장되었습니다.")
+else:
+    print(f"파일을 다운로드하는데 실패했습니다. 상태 코드: {response.status_code}")
 
 # Flask 로그 설정
 logging.basicConfig(level=logging.DEBUG)  # 모든 로그 레벨을 디버그로 설정
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-
-json_file_path = '../static/concert.json'
 
 
 @app.route("/")
